ce977b0b870c3459a40dc1d914b5368dc0f20ab7
anchit-goel <anchitgoel5@gmail.com>
2026-02-11 19:22:30 +0530

    Update footer links and developer info

--- js/main.js
+++ js/main.js
@@ -124,7 +124,15 @@ function loadFooter() {
                     <ul class="list-unstyled vstack gap-2">
                         <li class="d-flex align-items-center justify-content-between">
                             <span class="text-white">Anchit Goel</span>
-                            <a href="#" class="footer-icon-circle" aria-label="Anchit Goel LinkedIn">
+                            <a href="https://www.linkedin.com/in/anchitgoel5/" class="footer-icon-circle" aria-label="Anchit Goel LinkedIn" target="_blank">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.21c.837 0 1.358-.554 1.358-1.248-.015-.708-.52-1.248-1.342-1.248-.822 0-1.358.54-1.358 1.248 0 .694.521 1.248 1.327 1.248h.015zm4.908 8.21V9.359c0-.216.016-.432.08-.586.173-.432.568-.88 1.232-.88.87 0 1.218.664 1.218 1.635v3.866h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169H7.451c.03.664 0 7.225 0 7.225h2.4z"/>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="d-flex align-items-center justify-content-between">
+                            <span class="text-white">Shouraya Sharma</span>
+                            <a href="https://www.linkedin.com/in/shouraya-sharma/" class="footer-icon-circle" aria-label="Shouraya Sharma LinkedIn" target="_blank">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
                                     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.21c.837 0 1.358-.554 1.358-1.248-.015-.708-.52-1.248-1.342-1.248-.822 0-1.358.54-1.358 1.248 0 .694.521 1.248 1.327 1.248h.015zm4.908 8.21V9.359c0-.216.016-.432.08-.586.173-.432.568-.88 1.232-.88.87 0 1.218.664 1.218 1.635v3.866h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169H7.451c.03.664 0 7.225 0 7.225h2.4z"/>
                                 </svg>
@@ -133,14 +141,6 @@ function loadFooter() {
                         <li class="d-flex align-items-center justify-content-between">
                             <span class="text-white">Ryan Gupta</span>
                             <a href="#" class="footer-icon-circle" aria-label="Ryan Gupta LinkedIn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
-                                    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.21c.837 0 1.358-.554 1.358-1.248-.015-.708-.52-1.248-1.342-1.248-.822 0-1.358.54-1.358 1.248 0 .694.521 1.248 1.327 1.248h.015zm4.908 8.21V9.359c0-.216.016-.432.08-.586.173-.432.568-.88 1.232-.88.87 0 1.218.664 1.218 1.635v3.866h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169H7.451c.03.664 0 7.225 0 7.225h2.4z"/>
-                                </svg>
-                            </a>
-                        </li>
-                        <li class="d-flex align-items-center justify-content-between">
-                            <span class="text-white">Shouraya Sharma</span>
-                            <a href="#" class="footer-icon-circle" aria-label="Shouraya Sharma LinkedIn">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
                                     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.21c.837 0 1.358-.554 1.358-1.248-.015-.708-.52-1.248-1.342-1.248-.822 0-1.358.54-1.358 1.248 0 .694.521 1.248 1.327 1.248h.015zm4.908 8.21V9.359c0-.216.016-.432.08-.586.173-.432.568-.88 1.232-.88.87 0 1.218.664 1.218 1.635v3.866h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169H7.451c.03.664 0 7.225 0 7.225h2.4z"/>
                                 </svg>
@@ -202,8 +202,8 @@ function loadFooter() {
                     <h6 class="text-warning fw-bold mt-4 mb-2">Previous Editions</h6>
                     <p class="text-white-50 mb-2">Explore past conferences.</p>
                     <ul class="list-unstyled footer-list">
-                        <li><a href="#" class="footer-link">CODE AI 2025 Website</a></li>
-                        <li><a href="#" class="footer-link">CODE AI 2024 Website</a></li>
+                        <li><a href="https://conference.manipal.edu/CODE-AI2025/" class="footer-link" target="_blank">CODE AI 2025 Website</a></li>
+                        <li><a href="https://conference.manipal.edu/CODE-AI2024/" class="footer-link" target="_blank">CODE AI 2024 Website</a></li>
                     </ul>
                 </div>
                 <div class="col-lg-3 col-md-6">
